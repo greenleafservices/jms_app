62f702e46c0c745ed05fadd40368bd34cb7bb350
John Stegman <greenleafservices.gls@gmail.com>
2017-07-29 11:29:56 -0400

    Error Handling Added

--- app.js
+++ app.js
@@ -7,19 +7,40 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 
-//const colors = [
-//  'red',
-//  'orange',
-//  'yellow',
-//  'green',
-//  'blue',
-//  'purple'
-//];
-
 app.set('view engine', 'pug');
 
+
+// ***************  Demo of app sequencing ***************
+//app.use((req, res, next) => { 
+//    req.message = 'This message made it!';
+//    next();
+//});
+//
+//app.use((req, res, next) => { 
+//    console.log(req.message);
+//    next();
+//});
+
+// *************** Error generator of testing error handler ************
+//app.use((req, res, next) => { 
+//    console.log("Hello");
+//    const err = new Error('Oh noes!');
+//    err.status = 500;
+//    next(err);
+//});
+
+app.use((req, res, next) => { 
+    console.log("world");
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if (name) {
+      res.render('index', { name });
+    } else {
+      res.redirect('/hello');
+    }
 });
 
 app.get('/cards', (req, res) => {
@@ -27,13 +48,40 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.render('hello', { name: req.cookies.username });
+  //res.render('hello', { name: req.cookies.username });
+  // res.render('hello');
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
 });
 
 app.post('/hello', (req, res) => {
   // send the cookie to the browser after form submission
   res.cookie('username', req.body.username);
-  res.render('hello', { name: req.body.username });
+  //res.render('hello', { name: req.body.username });
+  // redirect to index page
+  res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
 });
 
 app.listen(3000, () => {

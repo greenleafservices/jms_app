4e9ad6582a6c51cc7a63d0f6dfc8eeeeedf118c4
John Stegman <greenleafservices.gls@gmail.com>
2017-07-31 11:51:23 -0400

    Challenge 1 - hide the hint, add the prompt

--- routes/cards.js
+++ routes/cards.js
@@ -8,9 +8,36 @@ router.get('/:id', (req, res) => {
     const { id } = req.params;
     const text = cards[id][side];
     const { hint } = cards[id];
+    const { prompt } = cards[id];
+    const { points } = cards[id];
+    /*
+      Create an object to store the data to be passed to the page
+    */
+    const templateData = { text };
+  /* 
+  1. You can add properties to an object as needed  - we'll add the score,
+  points, side, hint and prompt proerties to the templateData object as we go through this process
+  
+  2. Notice that the data passed to the receiving template does not
+        have to have the same name as the data object from the json file
+        (score-points)
+        
+  */
+    templateData.score = points;
+
+    if (side === 'question') {
+        templateData.side = 'q';
+        templateData.hint = hint;
+      
+    /* You don't have to pass all the params - just the ones you want
+        to use in this request to render
+    */
+    } else {
+        //templateData.side = 'a';
+        templateData.prompt = prompt;
+    }
     
-    const templateData = { text, hint };
-    res.render('card', templateData);
+  res.render('card', templateData);
 });
 
 module.exports = router;

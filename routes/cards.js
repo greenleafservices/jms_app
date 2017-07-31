6172fedce3c54957f1198e2fe0b2f4badafe43ff
John Stegman <greenleafservices.gls@gmail.com>
2017-07-31 10:26:10 -0400

    Add query string to cards params

--- routes/cards.js
+++ routes/cards.js
@@ -4,11 +4,13 @@ const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
 router.get('/:id', (req, res) => {
+    const { side } = req.query;
+    const { id } = req.params;
+    const text = cards[id][side];
+    const { hint } = cards[id];
     
-  res.render('card',  { 
-        prompt: cards[req.params.id].question,
-        hint: cards[req.params.id].hint
-    });
+    const templateData = { text, hint };
+    res.render('card', templateData);
 });
 
 module.exports = router;

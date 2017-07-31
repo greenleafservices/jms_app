36e91986f819298432e29e9b5b47cec8dee4446c
John Stegman <greenleafservices.gls@gmail.com>
2017-07-31 10:16:48 -0400

    Added json data file and changed cards.js to access it

--- app.js
+++ app.js
@@ -9,74 +9,17 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
-// ***************  Demo of app sequencing ***************
-//app.use((req, res, next) => { 
-//    req.message = 'This message made it!';
-//    next();
-//});
-//
-//app.use((req, res, next) => { 
-//    console.log(req.message);
-//    next();
-//});
-
-// *************** Error generator of testing error handler ************
-//app.use((req, res, next) => { 
-//    console.log("Hello");
-//    const err = new Error('Oh noes!');
-//    err.status = 500;
-//    next(err);
-//});
-
-app.use((req, res, next) => { 
-    console.log("world");
-    next();
-});
-
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-      res.render('index', { name });
-    } else {
-      res.redirect('/hello');
-    }
-});
-
-app.get('/cards', (req, res) => {
-  res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is." });
-});
-
-app.get('/hello', (req, res) => {
-  //res.render('hello', { name: req.cookies.username });
-  // res.render('hello');
-  const name = req.cookies.username;
-  if (name) {
-    res.redirect('/');
-  } else {
-    res.render('hello');
-  }
-});
-
-app.post('/hello', (req, res) => {
-  // send the cookie to the browser after form submission
-  res.cookie('username', req.body.username);
-  //res.render('hello', { name: req.body.username });
-  // redirect to index page
-  res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/hello');
-});
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
 
 app.use((err, req, res, next) => {
   res.locals.error = err;
@@ -87,8 +30,3 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
 });
-
-app.get('/sandbox', (req, res) => {
-    res.render('sandbox');
-});
-
